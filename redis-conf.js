3d406fcb79b95f6b55bba6a27d8d154684578d3f
rds2151 <rds@localhost.localdomain>
2024-01-26 15:40:59 +0530

    Implement Redis

---
+++ redis-conf.js
@@ -0,0 +1,21 @@
+const { createClient } = require("redis");
+
+const client = createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_URL,
+        port: process.env.Redis_Port
+    }
+});
+
+client.on('error', err => console.log(`Redis Client Error ${err}`))
+
+client.connect()
+    .then(() => {
+        console.log("Connected to Redis server");
+    })
+    .catch((error) => {
+        console.error("Error connecting to Redis:", error);
+    });
+
+module.exports = client;
--- socket/user-socket-handler.js
+++ socket/user-socket-handler.js
@@ -1,52 +1,56 @@
-const Server = require('../services/Server')
-const sobj = new Server()
-const userConnected = new Map();
+const Server = require("../services/Server");
+const client = require("../redis-conf");
+const sobj = new Server();
 
 exports.addUserConnection = (socketId, phoneNumber) => {
-    if (!userConnected.has(phoneNumber)) {
-        userConnected.set(phoneNumber, { socketId });
-        console.log(`User connected: ${phoneNumber} - Socket ID: ${socketId}`);
-    } else {
-        const existingUser = userConnected.get(phoneNumber);
-        existingUser.socketId = socketId;
-        console.log(`User reconnected: ${phoneNumber} - Updated Socket ID: ${socketId}`);
-    }
+    client.hSet("Session", phoneNumber, socketId);
 };
 
-exports.deleteUserConnection = (socketId) => {
-    for (const [phoneNumber, user] of userConnected.entries()) {
-        if (user.socketId === socketId) {
-            userConnected.delete(phoneNumber);
-            console.log(`User disconnected: ${phoneNumber} - Socket ID: ${socketId}`);
-            break;
-        }
-    }
+exports.deleteUserConnection = async (socketId) => {
+    client
+        .hGetAll("Session")
+        .then((sessionData) => {
+            for (const phoneNumber in sessionData) {
+                if (socketId === sessionData[phoneNumber]) {
+                    client.hDel("Session", phoneNumber);
+                }
+            }
+        })
+        .catch((error) => {
+            console.error(error.message);
+        });
 };
 
 const getUser = (phoneNumber) => {
-    if (userConnected.has(phoneNumber)) {
-        return userConnected.get(phoneNumber).socketId;
-    } else {
-        return null;
-    }
+    return client.hGet('Session', phoneNumber)
 };
 
 exports.sendMessage = async (socket, data) => {
-    const socketId = getUser(data.from);
+    const socketId = await getUser(data.from);
     if (data.to.startsWith("room-")) {
         socket.to(data.to).emit("receive-message", data);
     } else if (socketId !== null) {
         await sobj.appendChat(data)
-        const receiver = getUser(data.to);
+        const receiver = await getUser(data.to);
         socket.to(receiver).emit("receive-message", data);
     }
 };
 
-exports.fetchChatMessage = async(detail) => {
+exports.fetchChatMessage = async (detail) => {
+    const key = `${detail.to}:${detail.from}`
     try {
-        const result = await sobj.fetchChatMessage(detail)
-        return result
+        let result = await client.EXISTS(key)
+
+        if (result) {
+            result = await client.get(key)  
+            result = JSON.parse(result)
+        } else {
+            result = await sobj.fetchChatMessage(detail);
+            client.setEx(key, 15 ,JSON.stringify(result))
+        }
+
+        return result;
     } catch (err) {
-        console.log(err.message)
+        console.log(err.message);
     }
-}
+};
