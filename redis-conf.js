ea340286aa294c7675723f0445a5834fb4d93066
rds2151 <rds@localhost.localdomain>
2024-01-26 15:45:21 +0530

    Implement Redis

--- redis-conf.js
+++ redis-conf.js
@@ -4,7 +4,7 @@ const client = createClient({
     password: process.env.REDIS_PASSWORD,
     socket: {
         host: process.env.REDIS_URL,
-        port: process.env.Redis_Port
+        port: process.env.REDIS_PORT
     }
 });
 
